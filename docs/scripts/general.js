da7bc5bc664e19b327ea11ea0386134421d01fbf
rzllmr <30299783+rzllmr@users.noreply.github.com>
2021-05-08 11:56:24 +0200

    Add parallax scrolling for background

--- docs/scripts/general.js
+++ docs/scripts/general.js
@@ -1,7 +1,14 @@
 
 (function($, window, document) {
   $(function() {
-    
-
+    function scrollBackground() {
+      const bars = $('div.bar');
+      const outer = $('div#outer');
+      const scrollMax = Math.floor(bars.height() - $(window).height());
+      const newPos = Math.round(outer[0].scrollTop / outer[0].scrollTopMax * scrollMax);
+      $('div.bar').css('background-position-y', - newPos + 'px');
+    }    
+    $('div#outer').scroll(scrollBackground);
+    scrollBackground();
   });
 }(window.jQuery, window, document));
